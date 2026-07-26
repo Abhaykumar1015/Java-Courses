ff9e7381761aa91f2d7afd0e1371e111408b1458
Abhaykumar1015 <abhaykumarvalley@gmail.com>
2026-07-26 14:24:40 +0530

    first commit

---
+++ Data Type/Hello.java
@@ -0,0 +1,14 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 127;
+        short sh=558;
+        long l=57677l;
+        float f=5.5f;
+        double d=5.6;
+        boolean b=true;
+        // System.out.println(num1);
+    }
+}
