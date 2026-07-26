7a51476af5c134ef6c70b127fc297ed970555285
Abhaykumar1015 <abhaykumarvalley@gmail.com>
2026-07-26 19:46:43 +0530

    Updated conversion_casting

--- Data Type/Hello.java
+++ Data Type/Hello.java
@@ -3,14 +3,14 @@ class Hello
 {
     public static void main(String a[])
     {
-        int num1 = 9;
-        byte by = 127;
-        short sh=558;
-        long l=57677l;
-        float f=5.5f;
-        double d=5.6;
-        boolean b=true;
-        char c = 'k';
-        System.out.println(num1);
+        // int num1 = 9;
+        // byte by = 127;
+        // short sh=558;
+        // long l=57677l;
+        // float f=5.5f;
+        // double d=5.6;
+        // boolean b=true;
+        // char c = 'k';
+        // System.out.println(num1);
     }
 }
