96abceed0fdf849ed8ae3dbace7e4f31a4340bad
Abhaykumar1015 <abhaykumarvalley@gmail.com>
2026-07-27 13:41:30 +0530

    operators

---
+++ 04_Operator/operator.java
@@ -0,0 +1,42 @@
+class Operator
+{
+    public static void main(String[] args) {
+        
+        // int num1 = 7;
+        // int num2 = 5;
+        // int result= num1+num2;
+        // int result= num1-num2;
+        // int result= num1/num2;
+        // int result= num1%num2;
+        // System.out.println(result);
+        // int num1=7;
+        // num1=num1+2;
+        // num1+=2;
+        // num1-=2;
+        // num1/=7;
+        // num1*=8;
+        // System.out.println(num1);
+
+
+        // Increment Term //
+
+        int num=7;
+
+        // num++;  // Post increment
+        // num--;   // Post Decrement
+        // ++num;  // Pre Increment
+        // --num;  // Pre Decrement
+        // System.out.println(num);
+        
+        // int result=++num; // Fetch the value and then  increment
+        int result=num++; // increment the valuue and then fetch
+        // int term=result;
+        // System.out.println(term);
+        System.out.println(result);
+
+
+
+
+
+    }
+}
